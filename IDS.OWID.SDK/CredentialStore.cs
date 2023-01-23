bbc79c111e0ba573bf0bcb2a2c4c93fc67fa1e6b
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2023-01-23 23:54:21 +0100

    // commit

---
+++ IDS.OWID.SDK/CredentialStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace IDS.OWID.SDK
+{
+  public static class CredentialStore
+  {
+    public static void SaveCredentials(byte[] data, string password, string path) 
+      => File.WriteAllBytes(path, ProtectedData.Protect(data, Encoding.UTF8.GetBytes(password), DataProtectionScope.CurrentUser));
+
+    public static byte[] LoadCredentials(string password, string path)
+      => ProtectedData.Unprotect(File.ReadAllBytes(path), Encoding.UTF8.GetBytes(password), DataProtectionScope.CurrentUser);
+  }
+}
